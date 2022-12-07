76a97128a8a674803b0f2cca7bb7ef0b350cc7a1
Paucobacho <pau.cobacho01@estudiant.upf.edu>
2022-12-07 18:22:06 +0100

    third commit

--- ex3.py
+++ ex3.py
@@ -1,3 +1,19 @@
 # script to livestream with ffmpeg (any protocol you want) and open it
 # from mobile or any other device in the same network
 
+import os
+
+source = 'resources/BBB.mp4'
+
+
+def stream(video_path):
+
+    ip = input('Type an IP (same LAN) to send the stream to: ')
+    print('Streaming video to {0}:23000 through udp ...'.format(ip))
+    print('--> execute ffplay://127.0.0.1:2300 on the receiver')
+    cmd = 'ffmpeg -re -i {0} -c:v copy -loglevel quiet -f mpegts udp://{1}:23000'.format(video_path, ip)
+
+    os.system(cmd)
+
+
+stream(source)
